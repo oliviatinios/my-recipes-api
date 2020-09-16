c06dc02b7b26a11f96c76775f3509bc372780c03
Olivia Tinios <oliviatinios@gmail.com>
2020-09-16 17:37:55 -0400

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-const express = require("express");
-require("./db/mongoose");
-const userRouter = require("./routers/user");
-const recipeRouter = require("./routers/recipe");
-
-const app = express();
+const app = require("./app");
 const port = process.env.PORT;
-
-// Configure express to automatically parse the request body
-app.use(express.json());
-
-// Register routers
-app.use(userRouter);
-app.use(recipeRouter);
 
 // Register listener
 app.listen(port, () => {
