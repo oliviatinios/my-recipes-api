2b7811fc59d038cc20b6aeb2b71a9d1853cfaf29
Olivia Tinios <oliviatinios@gmail.com>
2020-09-17 09:59:30 -0400

    Update recipe test suite

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const User = require("../../src/models/user");
+const Recipe = require("../../src/models/recipe");
 
 // Define test user
 const userOneId = new mongoose.Types.ObjectId();
@@ -16,16 +17,69 @@ const userOne = {
   ],
 };
 
-const populateDatabase = async () => {
-  // Delete all users in database before running tests
-  await User.deleteMany();
+// Define test user
+const userTwoId = new mongoose.Types.ObjectId();
+const userTwo = {
+  _id: userTwoId,
+  name: "Cam",
+  email: "cam@example.com",
+  password: "Red000@!",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userTwoId }, process.env.JWT_SECRET),
+    },
+  ],
+};
 
-  // Save test user to database
+// Define test recipe
+const recipeOne = {
+  _id: new mongoose.Types.ObjectId(),
+  title: "Pesto Pasta",
+  description: "Pasta with pesto",
+  totalTime: 30,
+  owner: userOneId,
+};
+
+// Define test recipe
+const recipeTwo = {
+  _id: new mongoose.Types.ObjectId(),
+  title: "Grilled Cheese",
+  description: "Bread and cheese fried in a pan",
+  totalTime: 15,
+  owner: userOneId,
+};
+
+// Define test recipe
+const recipeThree = {
+  _id: new mongoose.Types.ObjectId(),
+  title: "Vegetable Fried Rice",
+  description: "Fried rice with vegetables",
+  totalTime: 30,
+  owner: userTwoId,
+};
+
+const populateDatabase = async () => {
+  // Delete all users and recipes in database before running tests
+  await User.deleteMany();
+  await Recipe.deleteMany();
+
+  // Save test users to database
   await new User(userOne).save();
+  await new User(userTwo).save();
+
+  //Save test recipes to database
+  await new Recipe(recipeOne).save();
+  await new Recipe(recipeTwo).save();
+  await new Recipe(recipeThree).save();
 };
 
 module.exports = {
   userOneId,
   userOne,
+  userTwoId,
+  userTwo,
+  recipeOne,
+  recipeTwo,
+  recipeThree,
   populateDatabase,
 };
--- tests/recipe.test.js
+++ tests/recipe.test.js
@@ -1,7 +1,14 @@
 const request = require("supertest");
 const app = require("../src/app");
 const Recipe = require("../src/models/recipe");
-const { userOneId, userOne, populateDatabase } = require("./fixtures/db");
+const {
+  userOneId,
+  userOne,
+  userTwoId,
+  userTwo,
+  recipeOne,
+  populateDatabase,
+} = require("./fixtures/db");
 
 beforeEach(populateDatabase);
 
@@ -18,5 +25,28 @@ test("Should create recipe for user", async () => {
 
   // Assert that the database was changed correctly
   const recipe = await Recipe.findById(response.body._id);
+  expect(recipe).not.toBeNull();
+});
+
+test("Should get all recipes for user", async () => {
+  const response = await request(app)
+    .get("/recipes")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+
+  // Assert that user one has two recipes
+  expect(response.body.length).toBe(2);
+});
+
+test("Should not delete task not owned by user", async () => {
+  const response = await request(app)
+    .delete(`/recipes/${recipeOne._id}`)
+    .set("Authorization", `Bearer ${userTwo.tokens[0].token}`)
+    .send()
+    .expect(404);
+
+  // Assert that recipe one still exists in database
+  const recipe = await Recipe.findById(recipeOne._id);
   expect(recipe).not.toBeNull();
 });
